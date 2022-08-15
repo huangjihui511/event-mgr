94648a171684f8b3725b9686a4c644edef2eec30
Jihui <huangjihui511@hotmail.com>
2022-08-16 00:26:53 +0800

    log bug

--- pkg/logs/logs.go
+++ pkg/logs/logs.go
@@ -1,7 +1,15 @@
 package logs
 
 import (
+	"os"
+
 	"github.com/sirupsen/logrus"
 )
 
-var Logger = logrus.New()
+var Logger *logrus.Logger
+
+func init() {
+	Logger = logrus.StandardLogger()
+	logrus.SetOutput(os.Stdout)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
