984b26d76219e8e3596a7cecd8c7de0f477162c3
Jihui <huangjihui511@hotmail.com>
2022-08-23 23:29:01 +0800

    tested

--- pkg/event/event_mgr.go
+++ pkg/event/event_mgr.go
@@ -4,38 +4,20 @@ import (
 	"context"
 	"fmt"
 	eventInterface "huangjihui511/event-mgr/pkg/event/interfaces"
-	"huangjihui511/event-mgr/pkg/event/timer"
 	"huangjihui511/event-mgr/pkg/logs"
 	"huangjihui511/event-mgr/pkg/notify"
 	notifyInterface "huangjihui511/event-mgr/pkg/notify/interfaces"
 	"huangjihui511/event-mgr/pkg/utils"
-	"huangjihui511/event-mgr/pkg/watcher/devops"
-	"huangjihui511/event-mgr/pkg/watcher/scb"
-	"huangjihui511/event-mgr/pkg/watcher/zsb"
-	"time"
 )
 
 var (
-	events       []eventInterface.Interface
-	notifyEmail  notifyInterface.EmailInterface
-	targetEmails = []string{
-		"717655909@qq.com",
-	}
+	events      []eventInterface.Interface
+	notifyEmail notifyInterface.EmailInterface
 )
 
 func init() {
 	notifyEmail = notify.EmailSender{
 		EmailMeta: notifyInterface.EmailMetaQQ,
-	}
-}
-
-func registerEvents() {
-	events = []eventInterface.Interface{
-		timer.NewTimer(time.Second, devops.NewWatcherBorn()),
-		timer.NewTimer(time.Minute*30, scb.NewWatcherExchangeRatio("SCB Watcher")),
-		timer.NewTimer(time.Minute, scb.NewWatcherExchangeRatioLowerBuyRatio("SCB Low Bound Watcher 6.8", 6.8)),
-		timer.NewTimer(time.Minute*30, zsb.NewWatcherExchangeRatio("ZSB Watcher")),
-		timer.NewTimer(time.Minute, zsb.NewWatcherExchangeRatioLowerBuyRatio("ZSB Low Bound Watcher 6.8", 6.8)),
 	}
 }
 
@@ -83,6 +65,13 @@ func do(ctx context.Context, ev eventInterface.Interface, index int) {
 	DashboardData.Items[index].LastNotifyAt = utils.TimeNow()
 	subject := fmt.Sprintf("%v: \"%v\"", ev.Watcher().Name(), r.Subject())
 	for _, t := range targetEmails {
+		ri := RouterItem{
+			EventName: ev.Watcher().Name(),
+			Email:     t,
+		}
+		if !RouterItems[ri] {
+			continue
+		}
 		err := notifyEmail.Send(t, subject, r.Msg())
 		if err != nil {
 			logs.Logger.Errorf("send email to %v failed: %s", t, err)
